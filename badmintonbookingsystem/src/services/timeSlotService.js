b914a2f625c290a2328f3371a0946e5edb97b31b
Dương Thị Trúc Linh <duongthitruclinh257@gmail.com>
2024-07-24 16:55:22 +0700

    Update timeSlotService.js

--- badmintonbookingsystem/src/services/timeSlotService.js
+++ badmintonbookingsystem/src/services/timeSlotService.js
@@ -1,4 +1,16 @@
-import { getTimeSlotsByCourtId } from "../api/apiTimeSlot";
+import { getTimeSlotsByCourtId, getTimeSlotbyCourtId } from "../api/apiTimeSlot";
+
+export const fetchTimeSlot = async (courtId, chosenDate) => {
+  try {
+    const response = await getTimeSlotbyCourtId(courtId, chosenDate);
+    const data = response.data;
+    // console.log(data);
+    return data;
+  } catch (error) {
+    console.error('Error fetching time slot:', error);
+    throw error;
+  }
+};
 
 export const fetchTimeSlotsByCourtId = async (courtId) => {
     try {
